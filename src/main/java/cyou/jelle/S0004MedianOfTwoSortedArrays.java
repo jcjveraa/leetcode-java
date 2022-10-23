d695e14bcb631879d3d318eca7f93a06d9b09ba5
JelleV <3942301+jcjveraa@users.noreply.github.com>
2022-10-23 16:11:45 +0200

    Dirty checkin to continue later

--- src/main/java/cyou/jelle/S0004MedianOfTwoSortedArrays.java
+++ src/main/java/cyou/jelle/S0004MedianOfTwoSortedArrays.java
@@ -16,10 +16,10 @@ public class S0004MedianOfTwoSortedArrays {
             fillArrays(one, two);
             long start = System.nanoTime();
             sol.findMedianSortedArrays(one, two);
-            linearTime += ((System.nanoTime() - start)/1e9d) ;
+            linearTime += ((System.nanoTime() - start) / 1e9d);
             start = System.nanoTime();
             sol.findMedianSortedArraysWithBinarySearch(one, two);
-            binarySearchtime += ((System.nanoTime() - start)/1e9d);
+            binarySearchtime += ((System.nanoTime() - start) / 1e9d);
         }
 
         System.out.println("Linear: " + linearTime);
@@ -27,27 +27,26 @@ public class S0004MedianOfTwoSortedArrays {
 
     }
 
-   private static void fillArrays(int[] one, int[] two) {
-        int i=0, j=0;
+    private static void fillArrays(int[] one, int[] two) {
+        int i = 0, j = 0;
         int value = 1;
         Random random = new Random();
         while (i < one.length && j < two.length) {
             var arrayPicker = random.nextInt(2);
-            value += random.nextInt(1,5);
-            if(arrayPicker == 0) {
+            value += random.nextInt(1, 5);
+            if (arrayPicker == 0) {
                 one[i++] = value;
-            } else
-            {
+            } else {
                 two[j++] = value;
             }
         }
 
         while (i < one.length) {
-            value += random.nextInt(1,5);
+            value += random.nextInt(1, 5);
             one[i++] = value;
         }
         while (j < two.length) {
-            value += random.nextInt(1,5);
+            value += random.nextInt(1, 5);
             two[j++] = value;
         }
     }
@@ -94,8 +93,58 @@ public class S0004MedianOfTwoSortedArrays {
         toPush[0] = newValue;
     }
 
-    private void findMedianSortedArraysWithBinarySearch(int[] one, int[] two) {
+    public double findMedianSortedArraysWithBinarySearch(int[] nums1, int[] nums2) {
+        int numNumbers = nums1.length + nums2.length;
+        boolean isEven = numNumbers % 2 == 0;
+        // Where is the midpoint - in case of e.g. 4 numbers in total, we need index 1 (& 2) for 2nd (& 3rd) number.
+        // With e.g. 7 numbers, the middle number is at index 3 exactly
+        int midpointMergedArray = isEven ? numNumbers / 2 - 1 : numNumbers / 2;
+
+        int[] small;
+        // find the smaller array
+        if (nums1.length == nums2.length) {
+            small = nums1[nums1.length - 1] < nums2[nums1.length - 1] ? nums1 : nums2;
+        } else {
+            small = nums1.length < nums2.length ? nums1 : nums2;
+        }
+
+        int[] large = small == nums1 ? nums2 : nums1;
+
+        // Binary search using knowledge we are looking for the middle of the 'merged' array
+
+        int small_l = 0;
+        int small_r = small.length - 1;
+        int small_midIndex = (small_r - small_l) / 2;
+        int largeIndex = midpointMergedArray - (small_midIndex + 1) - 1;
+
+
+
+
     }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
